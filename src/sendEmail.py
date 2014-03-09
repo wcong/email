742fef572ef7ada6095940d5998fc7a135395b07
cong <wc19920415@gamil.com>
2014-03-09 13:41:07 +0800

    first success

--- src/sendEmail.py
+++ src/sendEmail.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 #encoding=utf8
-from email.Header import Header
 from email.MIMEText import MIMEText
 from email.MIMEMultipart import MIMEMultipart
 import smtplib   
+import base64
 
 import conf
  
@@ -11,10 +11,15 @@ import conf
 #配置
 conf = conf.email_conf()
 
+#构造
+
+msg = MIMEMultipart()
 
 #邮件内容 
 contentFile = conf.content
-msg = MIMEText(open( contentFile ).read())
+msg.attach(MIMEText(open( contentFile ).read()))
+
+
 
 #添加附件， 特别是 文件名为中文时 需要转换为 gb2312
 fileList = conf.attacnemt
@@ -26,15 +31,22 @@ if len(  fileList  ) > 0:
         msg.attach(att)
 
 # header
-msg['to'] = conf.to
+msg['to'] = ','.join(conf.to)
 msg['from'] = conf.host
 msg['subject'] = conf.header
 
 
 #email 
 
-server = smtplib.SMTP()  
-server.connect(conf.server)  
+server = smtplib.SMTP_SSL(conf.server,465)  
+
+# 调试
+server.set_debuglevel(1)
+
+
+
 server.login(conf.user,conf.password)  
-server.sendmail( conf.host,conf.to,msg.to_string() );
+
+server.sendmail( conf.host,','.join(conf.to),msg.as_string() );
+
 server.close();
