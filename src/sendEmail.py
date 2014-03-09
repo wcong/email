32a774972e6453070bf70bfc1098c28150a4944a
cong <wc19920415@gamil.com>
2014-03-09 13:04:47 +0800

    update

--- src/sendEmail.py
+++ src/sendEmail.py
@@ -4,13 +4,17 @@ from email.Header import Header
 from email.MIMEText import MIMEText
 from email.MIMEMultipart import MIMEMultipart
 import smtplib   
+
+import conf
  
 
 #配置
-conf = email_conf()
+conf = conf.email_conf()
 
-# 附件
-msg = MIMEMultipart()
+
+#邮件内容 
+contentFile = conf.content
+msg = MIMEText(open( contentFile ).read())
 
 #添加附件， 特别是 文件名为中文时 需要转换为 gb2312
 fileList = conf.attacnemt
@@ -24,6 +28,13 @@ if len(  fileList  ) > 0:
 # header
 msg['to'] = conf.to
 msg['from'] = conf.host
-msg['subject'] = header
+msg['subject'] = conf.header
 
 
+#email 
+
+server = smtplib.SMTP()  
+server.connect(conf.server)  
+server.login(conf.user,conf.password)  
+server.sendmail( conf.host,conf.to,msg.to_string() );
+server.close();
